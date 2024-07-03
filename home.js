c716ca64bdc961f17359da088617cd07305a6bc0
u25754 <157565194+u25754@users.noreply.github.com>
2024-07-03 20:30:39 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+gsap.from(".herosection h1", {
+    delay: 1,
+    y: 60,
+    durartion: 5,
+    opacity: 0
+})
+
+gsap.from(".herosection p", {
+    delay: 1,
+    y: 60,
+    durartion: 5,
+    opacity: 0
+})
